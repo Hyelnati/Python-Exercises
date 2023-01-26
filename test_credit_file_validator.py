615796305c19c50ec4c381fdfc801d93344fd40d
Hyelnati <117924540+Hyelnati@users.noreply.github.com>
2023-01-26 20:18:28 +0100

    Add files via upload

---
+++ test_credit_file_validator.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    class CreditCardTest(card):
+        def test_check_card_length(self):
+            sup = "438857601252626"
+            for i in sup:
+                result = check_card_length(card)
+            self.assertTrue(16)
+
+        # self.fail()
+
+        def test_check_card_type(self):
+            print()
+            result2 = check_card_type("4")
+            self.assertTrue("4")
+
+        # self.fail()
+
+        def test_add_even_numbers(self):
+            result3 = add_even_numbers([2, 3, 5])
+            inputs = [1, 2, 3, 5]
+            result4 = add_even_numbers(inputs)
+            self.assertEqual(result4, result3)
+
+        # self.fail()
+
+        def test_add_odd_number_step_3(self):
+            result5 = add_odd_number_step_3([3, 7, 9])
+            input2 = [1, 3, 6, 7]
+            result6 = add_odd_number_step_3(input2)
+            self.assertEqual()
